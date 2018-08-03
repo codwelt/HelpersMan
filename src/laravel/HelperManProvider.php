ad6df0bf8f15ac4c7ec7ca9549f710adcc3a28ea
FuriosoJack <iam@furiosojack.com>
2018-08-03 15:29:15 -0500

    Se hace correcion del la inclusion de los herlpers en el service provider

--- src/laravel/HelperManProvider.php
+++ src/laravel/HelperManProvider.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace Codwelt\HelpersMan\laravel;
+use Illuminate\Support\ServiceProvider;
 
 /**
  * Class HelpersManProvider
  * @author FuriosoJack <iam@furiosojack.com>
  */
-class HelperManProvider extends \Illuminate\Support\ServiceProvider
+class HelperManProvider extends ServiceProvider
 {
     public function boot()
     {
