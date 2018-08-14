390cb706445c002e285e4d983237c8e738d1780e
FuriosoJack <iam@furiosojack.com>
2018-08-14 08:48:43 -0500

    Se añade nuevo metodo para purificar string

--- src/functions/basic.php
+++ src/functions/basic.php
@@ -17,5 +17,20 @@ if(!function_exists('helperman_random_string')){
         }
         return $str;
     }
+}
+
+if(!function_exists('helperman_purificate_string')){
+
+    /**
+     * Elimina caracteres especiales saltos de linea y cualquier cosa que no sea una letra o numero
+     * @param $string
+     * @param string $remplaceTo
+     * @param string $regex
+     * @return string
+     */
+    function helperman_purificate_string($string,$remplaceTo = '' ,$regex = '/[^A-Za-z0-9]/'):string
+    {
+        return preg_replace($regex,$remplaceTo,$string);
+    }
 
 }
