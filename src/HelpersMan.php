73d1af196612f7ca5196741fdca19be15de534cd
Juan Carlos <disenoweb@tae-ltda.com>
2020-01-03 11:37:57 -0500

    Se añade nueva funcion

--- src/HelpersMan.php
+++ src/HelpersMan.php
@@ -90,4 +90,58 @@ class HelpersMan
 
     }
 
+    /**
+     * Se encarga de eliminar todos los aceptos del string ingresado
+     * @param string $string
+     * @return mixed|string
+     */
+    public static function delete_acents($string = '')
+    {
+        $string = trim($string);
+        $string = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $string
+        );
+        $string = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $string
+        );
+        $string = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $string
+        );
+        $string = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $string
+        );
+        $string = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $string
+        );
+        $string = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C',),
+            $string
+        );
+        //Esta parte se encarga de eliminar cualquier caracter extraño
+        $string = str_replace(
+            array('"\"', "¨", "º", " - ", "~",
+                "#", "@", "|", "!", '"',
+                "· ", "$", " % ", " & ", " / ",
+                "(", ")", " ? ", "'", "¡",
+                "¿", "[", "^", "<code>", "]",
+                "+", "}", "{", "¨", "´",
+                ">", "< ", ";", ",", ":",
+                ".", " "),
+            '',
+            $string
+        );
+        return $string;
+    }
+
 }
