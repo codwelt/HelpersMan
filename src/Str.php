0a4f4fe5db99cd6a323584dc0c6e61db92aa3119
FuriosoJack <iam@furiosojack.com>
2021-07-06 08:50:23 -0500

    Se añade nuevos helpers

---
+++ src/Str.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Codwelt\HelpersMan;
+
+class Str
+{
+
+    /**
+     * Convert the given string to title case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function title($value)
+    {
+        return mb_convert_case($value, MB_CASE_TITLE, 'UTF-8');
+    }
+    
+    /**
+     * Convert the given string to upper-case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function upper($value)
+    {
+        return \mb_strtoupper($value, 'UTF-8');
+    }
+
+    /**
+     * Convert the given string to lower-case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function lower($value)
+    {
+        return \mb_strtolower($value, 'UTF-8');
+    }
+
+
+    /**
+     * Limit the number of characters in a string.
+     *
+     * @param  string  $value
+     * @param  int  $limit
+     * @param  string  $end
+     * @return string
+     */
+    public static function limit($value, $limit = 100, $end = '...')
+    {
+        if (\mb_strwidth($value, 'UTF-8') <= $limit) {
+            return $value;
+        }
+
+        return \rtrim(\mb_strimwidth($value, 0, $limit, '', 'UTF-8')) . $end;
+    }
+
+    /**
+     * Extrae lo numeros
+     * @param string $numbers
+     * @return string
+     */
+    public static function extractNumbers($numbers)
+    {
+        if (is_null($numbers)) {
+            return "0";
+        }
+        return (string)\preg_replace('/[^0-9]+/', '', $numbers);
+    }
+}
