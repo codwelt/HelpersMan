b2d978f646b520398c7858fcc6dafb07fd23601a
FuriosoJack <iam@furiosojack.com>
2021-07-06 09:03:37 -0500

    Se hace actualizacion y se añade nueva funcion

--- src/Str.php
+++ src/Str.php
@@ -2,6 +2,7 @@
 
 namespace Codwelt\HelpersMan;
 
+use voku\helper\ASCII;
 class Str
 {
 
@@ -15,7 +16,7 @@ class Str
     {
         return mb_convert_case($value, MB_CASE_TITLE, 'UTF-8');
     }
-    
+
     /**
      * Convert the given string to upper-case.
      *
@@ -67,5 +68,45 @@ class Str
             return "0";
         }
         return (string)\preg_replace('/[^0-9]+/', '', $numbers);
+    }
+
+    /**
+     * Transliterate a UTF-8 value to ASCII.
+     *
+     * @param  string  $value
+     * @param  string  $language
+     * @return string
+     */
+    public static function ascii($value, $language = 'en')
+    {
+        return ASCII::to_ascii((string) $value, $language);
+    }
+
+    /**
+     * Generate a URL friendly "slug" from a given string.
+     *
+     * @param  string  $title
+     * @param  string  $separator
+     * @param  string|null  $language
+     * @return string
+     */
+    public static function slug($title, $separator = '-', $language = 'en')
+    {
+        $title = $language ? static::ascii($title, $language) : $title;
+
+        // Convert all dashes/underscores into separator
+        $flip = $separator === '-' ? '_' : '-';
+
+        $title = preg_replace('![' . preg_quote($flip) . ']+!u', $separator, $title);
+
+        // Replace @ with the word 'at'
+        $title = str_replace('@', $separator . 'at' . $separator, $title);
+
+        // Remove all characters that are not the separator, letters, numbers, or whitespace.
+        $title = preg_replace('![^' . preg_quote($separator) . '\pL\pN\s]+!u', '', static::lower($title));
+
+        // Replace all separator characters and whitespace by a single separator
+        $title = preg_replace('![' . preg_quote($separator) . '\s]+!u', $separator, $title);
+
     }
 }
